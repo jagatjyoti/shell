3abc973713a7b0d191dfb8d98c01c161342b3024
jagat <jagatjyoti0@gmail.com>
2016-08-16 23:07:14 +0530

    Final commit

--- wordsearch.sh
+++ wordsearch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -p "Enter file to be searched: " $file
-read -p "Enter the word you want to search for: " $word
+read -p "Enter file to be searched: " file
+read -p "Enter the word you want to search for: " word
 
-count=$(grep -o "^${word}:" $file | wc -l)
-echo "The count for `$word`: " $count
+count=$(grep -o -e "$word" "$file" | wc -l)
+echo "The count for $word: $count"
